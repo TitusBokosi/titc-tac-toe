e3e172575e6269677facd1e1b9d5e83b30928e79
TitusBokosi <titusbokosi@gmail.com>
2025-01-17 18:59:45 +0200

    select spot, select winner functions

--- script.js
+++ script.js
@@ -1,16 +1,62 @@
-function gamePad (){
-    const myGamePad = Array(3).fill(Array(3).fill(0));
-    myGamePad[0][1] = 'player';
-    return{myGamePad};
+const MygamePad = ( function(){
+    const gamePad = new Array(9);
+    return {gamePad};
+})();
+
+function createPlayer(name){
+    return{name}
 }
 
-function createPlayer (name){
-    let score = 0;
-    const updateScore = () => score++;
-    const getScore = () => score;
-    return{name, updateScore, getScore};
+MygamePad.gamePad[9] = 6;
+
+const playerOne = createPlayer('player one');
+const playerTwo = createPlayer('player two');
+
+function selectWinner(){
+  if(MygamePad.gamePad[0] === MygamePad.gamePad[1] && MygamePad.gamePad[1] === MygamePad.gamePad[2]){
+    return true;
+  }
+  else if (MygamePad.gamePad[3] === MygamePad.gamePad[4] && MygamePad.gamePad[4] === MygamePad.gamePad[5]){
+    return true;
+  }
+  else if (MygamePad.gamePad[6] === MygamePad.gamePad[7] && MygamePad.gamePad[7] === MygamePad.gamePad[8]){
+    return true;
+  }
+  else if (MygamePad.gamePad[0] === MygamePad.gamePad[3] && MygamePad.gamePad[3] === MygamePad.gamePad[6]){
+    return true;
+  }
+  else if (MygamePad.gamePad[0] === MygamePad.gamePad[4] && MygamePad.gamePad[4] === MygamePad.gamePad[8]){
+    return true;
+  }
+  else if (MygamePad.gamePad[2] === MygamePad.gamePad[4] && MygamePad.gamePad[4] === MygamePad.gamePad[6]){
+    return true;
+  }
+  else if(MygamePad.gamePad[2] === MygamePad.gamePad[5] && MygamePad.gamePad[5] === MygamePad.gamePad[8]){
+    return true;
+  }
+  else if (MygamePad.gamePad[1] === MygamePad.gamePad[4] && MygamePad.gamePad[4] === MygamePad.gamePad[7]){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
-const playerOne = createPlayer('tee');
-playerOne.updateScore();
-console.log(playerOne.getScore())
+function selectSpot(player, index){
+
+    if(MygamePad.gamePad[index] === undefined){
+     ( MygamePad.gamePad[index] = player.name);
+    }
+    else{
+        return 'spot already selected';
+    }
+}
+
+MygamePad.gamePad[0] = playerOne.name;
+
+let try1 = selectSpot(playerTwo, 0);
+selectSpot(playerOne, 3);
+
+console.log(MygamePad.gamePad[3])
+
+
