52fec83023720e25e401e66db31b29a7ca3907bc
TitusBokosi <titusbokosi@gmail.com>
2025-01-22 16:35:45 +0200

    createPLayer function

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-const div1 =document.getElementById('spot-1');
-const h1 = document.querySelector('h1');
-div1.addEventListener('click', ()  =>{
-    h1.textContent = 'hi there';
-})
+const GameBoard = ( function() {
+    const myGameBoard = new Array(9);
+    
+    function pickSpot(currentPlayer, index){
+
+        if(myGameBoard[index] === undefined){
+            return myGameBoard[index] = currentPlayer.name;
+        }
+        else{
+            return alert('spot alredy taken');
+        }
+    }
+
+    
+    return {pickSpot};
+})();
+
+
+
+function CreatePlayer(name){
+
+    this.name = name;
+
+    return {name};
+}
+
