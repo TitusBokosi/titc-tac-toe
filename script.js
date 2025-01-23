2e03e42ef534da62fb1a06c171876183d02f2888
TitusBokosi <titusbokosi@gmail.com>
2025-01-23 13:45:51 +0200

    spot already taken issue

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const MygamePad = ( function() {
-    const gamePad = ['', '', '', '', '', '', '', '','']
+    const gamePad = new Array(9)
     
     function pickSpot(currentPlayer, index){
       gamePad[index] = currentPlayer.name;
@@ -23,14 +23,14 @@ let currentPlayer;
 
 function setCurrentPlayer(){
     
-
     if( counter % 2 === 0){
         currentPlayer = playerOne;
+        counter += 1;
     }
     else{
         currentPlayer = playerTwo;
+        counter += 1;
     }
-    counter++;
 }
 
 
@@ -63,11 +63,13 @@ const  spot9 = document.getElementById('spot-9');
 const play = document.getElementById('play');
 
 const spotArray = [spot1, spot2, spot3, spot4, spot5, spot6, spot7, spot8, spot9];
-setCurrentPlayer();
 
-spotArray.forEach(function(element, index){
+function loopThroughSpots(){
+  setCurrentPlayer();
+  display();
+  spotArray.forEach(function(element, index){
   element.addEventListener('click', () => {
-    if(MygamePad.gamePad[index] === ''){
+    if(MygamePad.gamePad[index] === undefined){
       MygamePad.gamePad[index] = currentPlayer.name;
       if(currentPlayer === playerOne){
         element.style.backgroundColor = 'red';
@@ -75,40 +77,23 @@ spotArray.forEach(function(element, index){
       else{
         element.style.background = 'blue';
       }
+      setCurrentPlayer();
+      display();
     }
     else{
       scoreDisplay.textContent = 'spot already taken';
     }
+    
   })
 })
-
-function game(){
-    setCurrentPlayer();
-    display();
-    spotEventListeners();
-    
 }
 
 
-function assignColor(target){
-
-  if(currentPlayer === playerOne){
-    target.style.backgroundColor = 'red';
-  }
-  else{
-    target.style.background = 'blue';
-  }
-
-}
 
 function display(){
-  if(MygamePad.selectWinner === true){
-    scoreDisplay.textContent = `${currentPlayer.name} has won`;
-  }
-  else{
     scoreDisplay.textContent = `${currentPlayer.name} your turn`;
-  }
+  
 }
 
-play.addEventListener('click', game);
+play.addEventListener('click', loopThroughSpots);
 
